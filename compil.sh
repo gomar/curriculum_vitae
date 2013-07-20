5fce0df971037f47e5195a9f96056d3742035288
Adrien Gomar (mac) <gomar@cerfacs.fr>
2013-07-20 23:03:19 +0200

    smatter compil.sh

--- compil.sh
+++ compil.sh
@@ -1,8 +1,7 @@
 #!/bin/tcsh -f
 
-set name = cv_en
-latexmk -xelatex $name.tex -f
-rm $name.aux $name.fdb_latexmk $name.fls $name.log $name.bbl $name.blg
-set name = cv_fr
-latexmk -xelatex $name.tex -f
-rm $name.aux $name.fdb_latexmk $name.fls $name.log $name.bbl $name.blg
+foreach file ($*)
+	set name = $file:r
+	latexmk -xelatex $name.tex -f
+	rm $name.aux $name.fdb_latexmk $name.fls $name.log $name.bbl $name.blg
+end
